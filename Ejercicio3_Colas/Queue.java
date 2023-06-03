c87966f4e72a532967367d4ca6cbf23961ae6295
klismannSis <kchancuana@unsa.edu.pe>
2023-06-03 13:29:36 -0500

    C:/Program Files/Git/Creando el metodo enqueue.

--- Ejercicio3_Colas/Queue.java
+++ Ejercicio3_Colas/Queue.java
@@ -9,3 +9,14 @@ public class Queue<T> { // Creando una clase Generica.
         this.P_Element = null; 
         this.Cola = null; // Null --> inidca que la cola esta vacia al inicio.
     }
+    public void enqueue(T data) {  // Establecemos el metodo "enqueue" , el cual se encarga de agregar un elemento a la cola. 
+        Node<T> newNode = new Node<>(data); // Crea y asigna un nuevo nodo a los datos existentes. 
+        if (isEmpty()) {
+        	P_Element = newNode;
+        	Cola = newNode;  // Se verifica si la cola está vacía y se agrega un nuevo nodo al final de la cola.
+        } else {
+        	Cola.setNextNode(newNode);
+            Cola = newNode;
+        }
+    }
+}
