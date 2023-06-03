1eb3092b4df251a22dda2354438678c64f5569d2
klismannSis <kchancuana@unsa.edu.pe>
2023-06-03 13:36:54 -0500

    C:/Program Files/Git/ Creando una cola de tipo Integer.

--- Ejercicio3_Colas/Main.java
+++ Ejercicio3_Colas/Main.java
@@ -1,22 +1,13 @@
 package EDA_LAB3.Ejercicio3_Colas;
-
-public class Main {
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		
-		Queue<Integer> queue = new Queue<>();
-		queue.enqueue(1);
-		queue.enqueue(2);
-		queue.enqueue(3);
-
-		System.out.println("Size: " + queue.size()); // Output: Size: 3
-		System.out.println("Front: " + queue.peek()); // Output: Front: 1
-
-		int element = queue.dequeue();
-		System.out.println("Dequeued: " + element); // Output: Dequeued: 1
-
-		System.out.println("Size: " + queue.size()); // Output: Size: 2
-		System.out.println("Front: " + queue.peek()); // Output: Front: 2
-
-	}
+    Queue<Integer> queue = new Queue<>(); // Se crea una cola de tipo Integer y para luego agragar elmentos.
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    queue.enqueue(4); // 6 Elementos
+    queue.enqueue(5);
+    queue.enqueue(6);
 	
+    }
 }
